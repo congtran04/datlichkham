051f42eb209d4bf9bd6fee4b26d2cd13c43baa2f
Cong Tran <congtranvan04@gmail.com>
2026-03-17 10:35:00 +0700

    fix start script

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.get("/lich", async (req,res)=>{
 
 })
 
-app.listen(3000,()=>{
-    console.log("Server chạy")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+  console.log("Server running")
 })
